36d9e8c1eae1f018a6b6dacd6dc8c099103304b8
Murduk <mert.ozdag@gmail.com>
2017-09-08 13:54:31 +0300

    test

--- main/udp_main.c
+++ main/udp_main.c
@@ -7,9 +7,6 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 
-
-
-
 /*
 udp_perf example
 
@@ -29,7 +26,6 @@ step3:
     you can see the info in serial output.
 */
 
-
 #include <errno.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -39,66 +35,67 @@ step3:
 
 #include "udp_perf.h"
 
-
 //this task establish a UDP connection and receive data from UDP
 static void udp_conn(void *pvParameters)
 {
     ESP_LOGI(TAG, "task udp_conn start.");
     /*wating for connecting to AP*/
-    xEventGroupWaitBits(udp_event_group, WIFI_CONNECTED_BIT,false, true, portMAX_DELAY);
+    xEventGroupWaitBits(udp_event_group, WIFI_CONNECTED_BIT, false, true, portMAX_DELAY);
     ESP_LOGI(TAG, "sta has connected to ap.");
-    
+
     /*create udp socket*/
     int socket_ret;
-    
+
 #if EXAMPLE_ESP_UDP_MODE_SERVER
     ESP_LOGI(TAG, "create udp server after 3s...");
     vTaskDelay(3000 / portTICK_RATE_MS);
     ESP_LOGI(TAG, "create_udp_server.");
-    socket_ret=create_udp_server();
+    socket_ret = create_udp_server();
 #else /*EXAMPLE_ESP_UDP_MODE_SERVER*/
     ESP_LOGI(TAG, "create udp client after 20s...");
     vTaskDelay(20000 / portTICK_RATE_MS);
     ESP_LOGI(TAG, "create_udp_client.");
     socket_ret = create_udp_client();
 #endif
-    if(socket_ret == ESP_FAIL) {
-	ESP_LOGI(TAG, "create udp socket error,stop.");
-	vTaskDelete(NULL);
+    if (socket_ret == ESP_FAIL)
+    {
+        ESP_LOGI(TAG, "create udp socket error,stop.");
+        vTaskDelete(NULL);
     }
-    
+
     /*create a task to tx/rx data*/
     TaskHandle_t tx_rx_task;
     xTaskCreate(&send_recv_data, "send_recv_data", 4096, NULL, 4, &tx_rx_task);
 
     /*waiting udp connected success*/
-    xEventGroupWaitBits(udp_event_group, UDP_CONNCETED_SUCCESS,false, true, portMAX_DELAY);
+    xEventGroupWaitBits(udp_event_group, UDP_CONNCETED_SUCCESS, false, true, portMAX_DELAY);
     int bps;
-    while (1) {
-	total_data = 0;
-	vTaskDelay(3000 / portTICK_RATE_MS);//every 3s
-	bps = total_data / 3;
+    while (1)
+    {
+        total_data = 0;
+        vTaskDelay(3000 / portTICK_RATE_MS); //every 3s
+        bps = total_data / 3;
 
-	if (total_data <= 0) {
-	    int err_ret = check_connected_socket();
-	    if (err_ret == -1) {  //-1 reason: low level netif error
-		ESP_LOGW(TAG, "udp send & recv stop.\n");
-		break;
-	    }
-	}
+        if (total_data <= 0)
+        {
+            int err_ret = check_connected_socket();
+            if (err_ret == -1)
+            { //-1 reason: low level netif error
+                ESP_LOGW(TAG, "udp send & recv stop.\n");
+                break;
+            }
+        }
 
 #if EXAMPLE_ESP_UDP_PERF_TX
-	ESP_LOGI(TAG, "udp send %d byte per sec! total pack: %d \n", bps, success_pack);
+        ESP_LOGI(TAG, "udp send %d byte per sec! total pack: %d \n", bps, success_pack);
 #else
-	ESP_LOGI(TAG, "udp recv %d byte per sec! total pack: %d \n", bps, success_pack);
+        ESP_LOGI(TAG, "udp recv %d byte per sec! total pack: %d \n", bps, success_pack);
 #endif /*EXAMPLE_ESP_UDP_PERF_TX*/
     }
     close_socket();
     vTaskDelete(tx_rx_task);
     vTaskDelete(NULL);
 }
-
-
 
 void app_main(void)
 {
